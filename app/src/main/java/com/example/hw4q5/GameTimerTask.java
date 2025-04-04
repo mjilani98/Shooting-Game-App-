e4d51deb293b4d1a3c66be86b82cd3bbc0d4dd89
mjilani98 <mjilani@emich.edu>
2025-04-04 14:17:13 -0400

    GameTimerTask.java : created a gamed timer task

--- app/src/main/java/com/example/hw4q5/GameTimerTask.java
+++ app/src/main/java/com/example/hw4q5/GameTimerTask.java
@@ -1,4 +1,24 @@
 package com.example.hw4q5;
 
-public class GameTimerTask {
+import java.util.TimerTask;
+
+public class GameTimerTask extends TimerTask
+{
+    private Game game;              //game model
+    private GameView gameView;      //game view
+
+    //constructor of game timer task class
+    public GameTimerTask(Game game,GameView gameView)
+    {
+        this.game = game;            //set game model
+        this.gameView = gameView;    //set game view
+    }
+
+    //run method of game timer task class
+    public void run()
+    {
+        game.update(); 
+
+        gameView.postInvalidate();      //redraw game view
+    }
 }

8e4040300949b2e96e0ec0d0080714d6b5c01fb6
mjilani98 <mjilani@emich.edu>
2025-04-04 14:13:03 -0400

    Game.java : created a gun

--- app/src/main/java/com/example/hw4q5/Game.java
+++ app/src/main/java/com/example/hw4q5/Game.java
@@ -6,9 +6,16 @@ public class Game
     private double sceneWidth ; //width of the screen
     private double sceneHeight; //Height of the scene
 
+    //ball coordinates
     private double ballX;   //x coordinate of the ball
     private double ballY;   //y coordinate of the ball
     private double ballSpeed; //speed of the ball
+
+    //gun coordinates
+    private double gunX ;
+    private double gunY;
+    private double gunAngle;
+    private double gunLength;
 
     private double radius;  //radius of the ball , bullet
     private boolean hit; //ball hit
@@ -20,6 +27,13 @@ public class Game
     {
         //create Initial game
         initializeGame();
+    }
+
+    //method update game model
+    public void update()
+    {
+        //move ball
+        moveBall();
     }
 
 
@@ -61,6 +75,23 @@ public class Game
         return ballSpeed;
     }
 
+    //get gun variables
+    public double getGunX() //get gun x
+    {
+        return gunX;
+    }
+    public double getGunY() //get gun y
+    {
+        return gunY;
+    }
+    public double getGunAngle() //get gun angle
+    {
+        return gunAngle;
+    }
+    public double getGunLength() //get gun length
+    {
+        return gunLength;
+    }
 
     //get scene variables
     public double getSceneHeight() //get tha scene height
@@ -87,9 +118,18 @@ public class Game
         this.sceneWidth = 1080;
 
         //initialize ball coordinates
-        this.ballX = sceneWidth - 50;
-        this.ballY = sceneHeight - 400 * Math.random() - 50;
-        this.ballSpeed = 10 + 10 * Math.random();
+        this.ballX = sceneWidth  * Math.random()-80;
+        this.ballY = sceneHeight - 55;
+        this.ballSpeed = 10 + 15 * Math.random();
+
+        //initialize gun coordinates
+        int min = 540; // minimum gun location on the screen
+        int max = 1070;// maximum gun location on the screen
+        this.gunY =  min + (int)(Math.random() * ((max - min + 1))); //generate a random location
+        this.gunX = 0;
+        this.gunLength = 200;
+        this.gunAngle = 0 ;
+
 
         //initialize radius - size of the ball/bullet
         this.radius = 50;

369a98cab73bc0dcc162f4e39513c902cf035f20
mjilani98 <mjilani@emich.edu>
2025-04-03 20:19:05 -0400

    GameView.java : Created background

--- app/src/main/java/com/example/hw4q5/GameView.java
+++ app/src/main/java/com/example/hw4q5/GameView.java
@@ -1,4 +1,47 @@
 package com.example.hw4q5;
 
-public class GameView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+public class GameView extends View
+{
+    private Game game;  //game model
+
+
+    //public constructor
+    public GameView(Context context, Game game)
+    {
+        super(context);
+
+        //set game model
+        this.game = game;
+    }
+
+    //method draws scene
+    public void onDraw(Canvas canvas)
+    {
+        //get scene dimensions
+        double sceneHeight = game.getSceneHeight();
+        double sceneWidth = game.getSceneWidth();
+
+        //get location of the ball
+        double ballX = game.getBallX();
+        double ballY = game.getBallY();
+
+
+        //get radius of ball/bullet
+        double radius = game.getRadius();
+
+
+        //create a paint object
+        Paint paint = new Paint();
+
+        //draw background
+        paint.setColor(Color.parseColor("#AAAAAA"));
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawRect(0, 0, (float)sceneWidth, (float)sceneHeight, paint);
+    }
 }
